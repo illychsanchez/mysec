871a8444b9ae6577df0799e0664f550fdbb128e2
illychsanchez <pram.patel@gmail.com>
2021-05-06 11:27:09 +0530

    Add files via upload

---
+++ tr.ps1
@@ -0,0 +1,22 @@
+# Config
+$Username = "PFA"
+$Password = "in9DFF@8"
+$LocalFile = "d:\log.txt"
+$RemoteFile = "ftp://ftp1.speedgaze.com/Success.txt"
+ 
+# Create FTP Rquest Object
+$FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
+$FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
+$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
+$FTPRequest.UseBinary = $true
+$FTPRequest.UsePassive = $true
+# Read the File for Upload
+$FileContent = gc -en byte $LocalFile
+$FTPRequest.ContentLength = $FileContent.Length
+# Get Stream Request by bytes
+$Run = $FTPRequest.GetRequestStream()
+$Run.Write($FileContent, 0, $FileContent.Length)
+# Cleanup
+$Run.Close()
+$Run.Dispose()
